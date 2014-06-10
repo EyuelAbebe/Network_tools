1abb4282058b5e2ce94821e5c9e824bcfdae0697
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-06-10 14:55:01 -0700

    Created the server and client python files. Also created echo_server_client for the bonus challenge

---
+++ echo_server.py
@@ -0,0 +1,33 @@
+import socket
+
+class Server():
+
+    def __init__(self, port = 3046):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+        self.server_socket.bind(('127.0.0.1', port))
+        self.server_socket.listen(2)
+
+    def serve(self):
+
+        while True:
+
+            buffer_size = 32
+            conn, client_address = self.server_socket.accept()
+
+            response = []
+            done = False
+            while not done:
+                recieved_message = conn.recv(buffer_size)
+                if not recieved_message:
+                    done = True
+
+                response.append(recieved_message)
+
+            response = ''.join(response)
+            conn.sendall(response)
+            conn.close()
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.serve()
---
+++ echo_server_client.py
@@ -0,0 +1,43 @@
+import sys
+import time
+import threading
+from echo_server import Server
+from echo_client import Client
+
+def client_send_receive(message):
+    _client = Client()
+    _client.send(message)
+    print _client.receive()
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self._target(*self._args)
+
+
+
+class ServerThread(threading.Thread):
+    def run(self):
+        return Server().serve()
+
+class Echo_Client_Server(threading.Thread):
+
+    def __init__(self, _func, message):
+        self.server = ServerThread().start()
+        self.client = ClientThread(_func, message).start()
+
+
+if __name__ == "__main__":
+    _client = Echo_Client_Server(client_send_receive, sys.argv[1])
+    time.sleep(1)
+
+    while True:
+        print "+"*100
+        _new_Client = ClientThread(client_send_receive, raw_input("Give me a new input to echo: ")).start()
+        time.sleep(2)
+
